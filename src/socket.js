73ab305b44768424f7e7f9def8373bcb1f9746f6
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-05 13:29:42 -0400

    Custom event 'message'

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,9 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log("Un usuario se ha conectado")
+        socket.on('message', (data) => {
+            console.log(data)
+        })
         socket.on('disconnect', () => {
             console.log('El usuario se ha desconectado')
         })
