3be516621f252686b6a4561e68e0c6c86aa89639
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-05 13:45:06 -0400

    Chating

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,11 @@
 module.exports = (io) => {
+    var messages = []
     io.on('connection', (socket) => {
         console.log("Un usuario se ha conectado")
+        io.emit('messages', messages)
         socket.on('message', (data) => {
-            console.log(data)
+            messages.push(data)
+            io.emit('messages', messages)
         })
         socket.on('disconnect', () => {
             console.log('El usuario se ha desconectado')
